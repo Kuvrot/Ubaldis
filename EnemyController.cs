c2e471f96ab1f642c6066e5a644eff82aaef33ae
Kuvrot <jamessanz@outlook.com>
2024-08-05 23:26:59 -0600

    Initial commit

---
+++ EnemyController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using Stride.Particles.Components;
+
+namespace Ubaldis
+{
+    public class EnemyController : SyncScript
+    {
+       public float speed = 0.5f;
+        public TransformComponent target;
+        public EntityComponent projectile;
+        public ParticleSystemComponent particleSystem;
+        public float stoppingDistance;
+
+        public override void Start()
+        {
+             particleSystem.ParticleSystem.Stop();
+        }
+
+        public override void Update()
+        {
+            stoppingDistance = Vector3.Distance(target.Position , Entity.Transform.Position);
+                
+                if (stoppingDistance <= 4)
+                {
+                    particleSystem.ParticleSystem.Play();
+                }
+                else
+                {
+                    Entity.Transform.Position.Y -= speed * (float)Game.UpdateTime.Elapsed.TotalSeconds;
+                }
+        }
+    }
+}
