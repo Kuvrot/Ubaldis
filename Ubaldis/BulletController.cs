c2c5b141c16d8bd5681b3b15bc7164f9c39ace24
Kuvrot <jamessanz@outlook.com>
2024-08-06 00:29:40 -0600

    bullet implementation

---
+++ Ubaldis/BulletController.cs
@@ -0,0 +1,36 @@
+﻿/*
+ This script only makes the bullet travel in positive or negative directions in the Y axis.
+ How bullets work: 
+                    The bullet only works as visual feedback, if you disable all the bullets the game should be working as usual.
+ 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+
+namespace Ubaldis
+{
+    public class BulletController : SyncScript
+    {
+        public bool direction = false; //false for negative, true for positive
+        public float speed = 50; 
+        
+        public override void Update()
+        {
+            if (direction)
+            {
+                Entity.Transform.Position.Y += speed * (float)Game.UpdateTime.Elapsed.TotalSeconds;
+            }
+            else
+            {
+                Entity.Transform.Position.Y -= speed * (float)Game.UpdateTime.Elapsed.TotalSeconds;
+            }
+        }
+    }
+}
--- Ubaldis/EnemyController.cs
+++ Ubaldis/EnemyController.cs
@@ -7,6 +7,7 @@ using Stride.Core.Mathematics;
 using Stride.Input;
 using Stride.Engine;
 using Stride.Particles.Components;
+using SharpFont.PostScript;
 
 namespace Ubaldis
 {
@@ -14,27 +15,54 @@ namespace Ubaldis
     {
        public float speed = 0.5f;
         public TransformComponent target;
-        public EntityComponent projectile;
+        public Prefab projectile;
         public ParticleSystemComponent particleSystem;
         public float stoppingDistance;
 
+        float timer = 2f;
+        float clock = 2f;
         public override void Start()
         {
              particleSystem.ParticleSystem.Stop();
         }
-
+    
         public override void Update()
         {
             stoppingDistance = Vector3.Distance(target.Position , Entity.Transform.Position);
                 
-                if (stoppingDistance <= 4)
+            if (stoppingDistance <= 4)
+            {
+                if (clock <= 0)
                 {
-                    particleSystem.ParticleSystem.Play();
+                    particleSystem.ParticleSystem.Play(); //Muzzle flash plays
+
+                    //Projectile instance
+                    var p = projectile.Instantiate();
+
+                    foreach (var entity in p)
+                    {
+                        if (entity.Transform != null)
+                        {
+                            entity.Transform.Position = Entity.Transform.Position;
+                            entity.Transform.Position.Z -= 10;
+                        }
+
+                        Entity.Scene.Entities.Add(entity);
+                    }
+
+                    particleSystem.ParticleSystem.ResetSimulation(); //Muzzle flash plays
+
+                    clock = timer;
                 }
                 else
                 {
-                    Entity.Transform.Position.Y -= speed * (float)Game.UpdateTime.Elapsed.TotalSeconds;
+                    clock -= 1 * (float)Game.UpdateTime.Elapsed.TotalSeconds;
                 }
+            }
+            else
+            {
+                Entity.Transform.Position.Y -= speed * (float)Game.UpdateTime.Elapsed.TotalSeconds;
+            }
         }
     }
 }
